d4b5183b3b1b1298014e754df0b9ceb1cdd122e3
Florian Thiery <web@florian-thiery.de>
2019-09-15 11:07:02 +0200

    add cattu icon

--- js/leaflet-color-markers.js
+++ js/leaflet-color-markers.js
@@ -76,3 +76,10 @@ var cattuIcon = new L.Icon({
 	iconAnchor: [20, 41],
 	popupAnchor: [0, -41]
 });
+
+var cunaIcon = new L.Icon({
+	iconUrl: 'img/CUNA-2x.png',
+	iconSize: [41, 41],
+	iconAnchor: [20, 41],
+	popupAnchor: [0, -41]
+});
